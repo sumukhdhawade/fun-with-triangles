95b038bfa96221e4a5d486854bb4101db76ed4c7
sumukhdhawade <sumukh.dhawade-coep@bvucoep.edu.in>
2021-12-10 05:20:01 +0530

    initial commit

---
+++ scripts/quiz.js
@@ -0,0 +1,25 @@
+const quizform = document.querySelector(".quiz-form");
+const submitBtn = document.querySelector("#submit-answers-btn");
+const outputEl = document.querySelector(".output");
+
+const correctAnswers = [
+  "90°",
+  "right angled",
+  "one right angle",
+  "45°",
+];
+
+function calculateScore() {
+  const formResults = new FormData(quizform);
+  let score = 0,
+    index = 0;
+  for (let value of formResults.values()) {
+    if (value === correctAnswers[index]) {
+      score = score + 1;
+    }
+    index = index + 1;
+  }
+  outputEl.innerText = "The score is " + score;
+}
+
+submitBtn.addEventListener("click", calculateScore);
